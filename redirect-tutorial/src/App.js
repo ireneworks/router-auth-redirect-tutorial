b36c2295f1936f1b1ded6eb498717697ed4fb2d6
ireneworks <hello@ireneworks.com>
2022-05-23 14:16:15 +0900

    update context usage

--- redirect-tutorial/src/App.js
+++ redirect-tutorial/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Dialogue from "./dialogue";
+import {nameContext} from "./nameContext";
 
-function App() {
+export function App() {
+    const context = nameContext;
+const name = "Daniel";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+          <h1>Hello, World!</h1>
+          <context.Provider value={name}>
+              <Dialogue/>
+          </context.Provider>
+      </>
+
   );
 }
 
